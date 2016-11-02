e88e33831d9c7ddff0d40d0dd78cacdc0af55fd4
Alex Kopen <alexkopen@gmail.com>
2016-11-02 00:13:24 -0500

    Less and more gulp testing

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,44 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var less = require('gulp-less');
+var cssmin = require('gulp-cssmin');
 
-var paths = {
-  scripts: 'src/*.js'
+var sourcePaths = {
+  scripts: 'src/js/*.js',
+  stylesheets: 'src/css/*.less'
+};
+
+var destinationPaths = {
+  scripts: 'dist/js',
+  stylesheets: 'dist/css'
 };
 
 gulp.task('scripts', function() {
-  return gulp.src(paths.scripts)
+  return gulp.src(sourcePaths.scripts)
     .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(concat('all.min.js'))
+	.pipe(uglify())
+	.pipe(concat('all.min.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest(destinationPaths.scripts));
+});
+
+gulp.task('less', function() {
+  return gulp.src(sourcePaths.stylesheets)
+  .pipe(sourcemaps.init())
+  .pipe(less().on('error', function (err) {
+    console.log(err);
+    this.emit('end');
+  }))
+  .pipe(cssmin())
+  .pipe(sourcemaps.write())
+  .pipe(concat('style.css'))
+  .pipe(gulp.dest(destinationPaths.stylesheets));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(sourcePaths.scripts, ['scripts']);
+  gulp.watch(sourcePaths.stylesheets, ['less']);
 });
 
-gulp.task('default', ['watch', 'scripts']);
+gulp.task('default', ['watch', 'scripts', 'less']);
