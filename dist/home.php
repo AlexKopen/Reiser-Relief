3219ac9522379e30163b3e451cc726d13281c754
Alex Kopen <alexkopen@gmail.com>
2016-11-06 22:30:42 -0600

    Home page additions

--- dist/home.php
+++ dist/home.php
@@ -3,5 +3,58 @@
 		<div id ="slide-show">
 			<img src="resources/images/bannerpic1.jpg" alt="Mission trip participants">
 		</div>
+		<div class="pure-g" id="information">
+			<div class="pure-u-1-3">
+				<div class="info-content">
+					<div class="text-block">
+						<p>
+							<span class="beginning">Mission</span> Reiser Relief aims to achieve providing relief, hope, and dignity to the poor, elderly, and homeless people of Haiti.
+						</p>
+						<p>
+							Join our mailing list today!
+						</p>
+					</div>
+					<div class="link-block">
+						<a href="">
+							<p>
+								SIGN UP
+							</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="pure-u-1-3">
+				<div class="info-content">
+					<div class="text-block">
+						<p>
+							<span class="beginning">Make a Difference</span> Feel free to check out our volunteer opportunities or even apply for a mission trip to Haiti!
+						</p>
+					</div>
+					<div class="link-block">
+						<a href="">
+							<p>
+								EXPERIENCE NOW
+							</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="pure-u-1-3">
+				<div class="info-content" id="last-info">
+					<div class="text-block">
+						<p>
+							<span class="beginning">Support Us</span> Reiser Relief is entirely based off of donations and 100% of our proceedings go to the people that need it most.
+						</p>
+					</div>
+					<div class="link-block">
+						<a href="">
+							<p>
+								GIVE
+							</p>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
