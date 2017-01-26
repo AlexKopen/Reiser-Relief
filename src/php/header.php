2e2a2fddfbad20e8ff557e5d2cc197d3c0c6abac
Alex Kopen <alexkopen@gmail.com>
2017-01-26 00:29:33 -0600

    Added assets variables

--- src/php/header.php
+++ src/php/header.php
@@ -1,11 +1,18 @@
+<?php
+	$assets = file_get_contents('assets.json');
+	$assetsJson = json_decode($assets, true);
+	$masterStylesheet = 'resources/css/' . $assetsJson['style.css'];
+	$masterScript = 'resources/js/' . $assetsJson['all.min.js'];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Reiser Relief - Home Page</title>
-	<link rel="stylesheet" href="resources/css/pure-min.css">
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo($masterStylesheet); ?>">
 
 </head>
 <body>
