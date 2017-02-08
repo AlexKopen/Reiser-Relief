7d8b7254dc4d152c15fdc5e3a3e1e68cd855167e
AlexKopen <alexkopen@gmail.com>
2017-02-07 23:15:27 -0600

    updated slide show

--- src/js/slideShow.js
+++ src/js/slideShow.js
@@ -1,33 +1,18 @@
 $( document ).ready(function() {
-    function beginSlideShow() {
-        setInterval(function() { 
-            $('#middle-image').animate({
-                left: "-=1000"
-            }, 4000, function() {
-                $('#middle-image img').get(1).setAttribute('id', '');
-                $('#middle-image img').get(0).remove();
-                $('#slide-show #middle-image').append('<img src=" ' + Images[SlideShowIndex] + '" id ="second-image">');
-                 $('#middle-image').css('left', '0');
-
-                if (++SlideShowIndex > MaxIndex) {
-                    SlideShowIndex = 0;
-                }
-            });
-        }, 9000);
-    }
-
-    var Images = [];
-    var SlideShowIndex = 2;
-    var MaxIndex;
-
-    $('#slide-show ul li').each(function(){
-        Images.push($(this).text().trim());
+    $('#slide-show .hide-slide').each(function(){
+        var ImageUrl = $(this).text().trim();
+        $(this).text('');
+        $(this).append('<img src="' + ImageUrl + '">');     
+        $(this).css('display', 'inline');
     });
 
-    $('#slide-show ul li').promise().done(function() {
-        MaxIndex = Images.length - 1
-        $('#slide-show #middle-image').append('<img src=" ' + Images[1] + '" id ="second-image">');
-        beginSlideShow();
+    $('#slide-show .hide-slide').promise().done(function() {
+        $('#slide-show').unslider({
+            autoplay: true,
+            infinite: true,
+            arrows: false,
+            speed: 1000,
+            delay: 6000
+        });
     });
-
 });
