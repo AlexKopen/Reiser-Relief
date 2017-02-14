5439e0cd2054e80c5535829bbd87f1990d94c6dc
Alex Kopen <alexkopen@gmail.com>
2017-02-14 15:08:44 -0600

    Slide show SEO and alt tags

--- src/js/slideShow.js
+++ src/js/slideShow.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
     $('#slide-show .hide-slide').each(function(){
-        var ImageUrl = $(this).text().trim();
-        $(this).text('');
-        $(this).append('<img src="' + ImageUrl + '">');     
+        var ImageUrl = $(this).find('.hidden-image-url').text().trim();
+        $(this).find('.hidden-image-url').remove();
+        $(this).find('img').attr('src', ImageUrl);
         $(this).css('display', 'inline');
     });
 
