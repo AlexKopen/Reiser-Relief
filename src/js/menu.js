ce8a956487ff86d45ad01fd20eb6b7faba4d5de2
AlexKopen <alexkopen@gmail.com>
2017-03-12 23:21:10 -0500

    Ul menu display fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,9 +3,17 @@ $( document ).ready(function() {
         $('header ul').toggle();
     });
 
+    var enteredFromDesktop = !Modernizr.mq('(max-width: 768px)') ? true : false;
+
     $(window).on('resize', function() {
         if (!Modernizr.mq('(max-width: 768px)')) {
+            enteredFromDesktop = true;
             $('header ul').css('display', 'block');
+        } else {
+            if (enteredFromDesktop) {
+                enteredFromDesktop = false;
+                $('header ul').css('display', 'none');
+            }
         }
     });
 });
