6edc1ca5fb2f7c970b545b523ae59970d9edb50f
Alex Kopen <alexkopen@gmail.com>
2017-06-09 00:17:46 -0500

    One good looking form

--- src/js/application.js
+++ src/js/application.js
@@ -1,32 +1,28 @@
 var application = (function () {
     'use strict';
 
-    function legalName() {
-        var first, middle, last;
-
-        $('#first, #middle, #last').on('input', function () {
-            first = $('#first').val();
-            middle = $('#middle').val();
-            last = $('#last').val();
-
-            $('#full-legal-name').text(first + ' ' + middle + ' ' + last);
+    function allCheckboxSelected() {
+        $('.pure-checkbox').change(function () {
+            if ($('#apply input:checked').length === 2) {
+                $('#apply button').prop('disabled', false);
+                $('#apply #error-message span').css('display', 'none');
+            } else {
+                $('#apply button').prop('disabled', true);
+            }
         });
     }
 
-    function allCheckboxSelected() {
-        $('.pure-checkbox').change(function () {
-            if($('input:checked').length === 2) {
-                $('button').prop('disabled', false);
-            } else {
-                $('button').prop('disabled', true);
+    function submitApp() {
+        $('#apply #submit-hold').click(function () {
+            if ($('#apply input:checked').length !== 2) {
+                $('#apply #error-message span').css('display', 'block');
             }
         });
     }
 
     function init() {
-        legalName();
         allCheckboxSelected();
-
+        submitApp();
     }
 
     var applicationModule = {
