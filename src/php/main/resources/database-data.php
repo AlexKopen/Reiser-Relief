ddf0e5530cd11e627f11abca8e4e5bae5371e74e
Alex Kopen <alex.kopen@optum.com>
2017-07-20 17:31:41 -0500

    Initial dummy database data

---
+++ src/php/main/resources/database-data.php
@@ -0,0 +1,15 @@
+<?php
+// Dummy database data.  Replace the array values with data from a database.
+
+$news = array(
+    array(
+        'Title' => 'Test Title 1',
+        'Date' => 'Test Date 1',
+        'Content' => 'Test Content 1'
+    ),
+    array(
+        'Title' => 'Test Title 2',
+        'Date' => 'Test Date 2',
+        'Content' => 'Test Content 2'
+    )
+);
