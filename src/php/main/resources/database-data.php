f6e7b220aa3b706d27939295cc10852719b237ab
Alex Kopen <alexkopen@gmail.com>
2017-07-23 16:35:48 -0500

    News data

--- src/php/main/resources/database-data.php
+++ src/php/main/resources/database-data.php
@@ -3,13 +3,22 @@
 
 $news = array(
     array(
-        'Title' => 'Test Title 1',
-        'Date' => 'Test Date 1',
-        'Content' => 'Test Content 1'
+        'Title' => 'Our Latest Newsletter',
+        'Date' => 'June 10, 2017',
+        'Content' => 'We are delighted to share the impact of your support in Haiti!  Click here to read our Summer 2017 newsletter.'
     ),
     array(
-        'Title' => 'Test Title 2',
-        'Date' => 'Test Date 2',
-        'Content' => 'Test Content 2'
-    )
+        'Title' => 'Join us! 11th Annual Keep the Wheel Turning Event',
+        'Date' => 'March 28, 2017',
+        'Content' => 'Thursday, April 27, 2017. 6-9pm. $10 admission at the door. Includes soft drinks and appetizers. Cash bar. Honor the legacy of Fr. Reiser!'
+    ),
+    array(
+        'Title' => 'Thank you!',
+        'Date' => 'December 3, 2016',
+        'Content' => 'We are grateful for all of you! Thank you Give to the Max Day supporters, volunteers and event attendees!'
+    ),
+    array(
+        'Title' => 'Hurricane Matthew Relief',
+        'Date' => 'October 4, 2016',
+        'Content' => 'Thank you for your support. Please pray for the people who suffered damage from this devastating storm.')
 );
