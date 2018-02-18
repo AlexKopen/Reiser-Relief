f36fe90b3607467762e841e4b19a55edc3955342
Alex Kopen <alexkopen@gmail.com>
2018-02-18 14:38:23 -0600

    simplified menu dropdown js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,36 +1,35 @@
 var menu = (function () {
-    'use strict';
+  'use strict';
 
-    function menuDrop() {
-        $('#drop-down-menu').on('click', function () {
-            $('header ul').slideToggle();
-        });
+  function menuDrop() {
+    $('#drop-down-menu').on('click', function () {
+      $('header ul').slideToggle();
+    });
+  }
+
+  function windowResize() {
+    if (Modernizr.mq('(max-width: 767px)')) {
+      $('header ul').css('display', 'none');
     }
 
-    function windowResize() {
-        var enteredFromDesktop = !Modernizr.mq('(max-width: 767px)');
-        $(window).on('resize', function () {
-            if (!Modernizr.mq('(max-width: 767px)')) {
-                enteredFromDesktop = true;
-                $('header ul').css('display', 'inline-block');
-            } else {
-                if (enteredFromDesktop) {
-                    enteredFromDesktop = false;
-                    $('header ul').css('display', 'none');
-                }
-            }
-        });
-    }
+    $(window).on('resize', function () {
+      if (!Modernizr.mq('(max-width: 767px)')) {
+        $('header ul').css('display', 'inline-block');
+      } else {
+        $('header ul').css('display', 'none');
+      }
+    });
+  }
 
-    function init() {
-        menuDrop();
-        windowResize();
-    }
+  function init() {
+    menuDrop();
+    windowResize();
+  }
 
-    var menuModule = {
-        init: init
-    };
+  var menuModule = {
+    init: init
+  };
 
-    return menuModule;
+  return menuModule;
 
 })();
