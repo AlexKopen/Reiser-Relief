a26fe00a6f8935c1ad413a2f993399ede9eeb191
Alex Kopen <alexkopen@gmail.com>
2018-03-05 16:07:14 -0600

    Slide adjustment and event first event display

--- src/js/event.js
+++ src/js/event.js
@@ -3,7 +3,7 @@ var event = (function () {
 
   function eventShow() {
     $('.individual-event h2').on('click', function () {
-      $(this).next('.event-content').slideToggle();
+      $(this).next('.event-content').slideToggle(800);
       $(this).find('.right-arrow, .down-arrow').toggle();
     });
   }
