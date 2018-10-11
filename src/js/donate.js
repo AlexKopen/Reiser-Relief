c79a0eacb21069e70eeb836d40eedeac6c8154a7
AlexKopen <alexkopen@gmail.com>
2018-10-10 19:44:02 -0600

    determine value to charge credit card

--- src/js/donate.js
+++ src/js/donate.js
@@ -38,6 +38,7 @@ var donate = (function () {
       activeDonationLevel.addClass('active');
     });
 
+    var amountToDonate;
     $('#donation-form .donation-level li').click(function () {
       $('#donation-form .error').css('visibility', 'hidden');
       $('#donation-form .donation-level li').each(function() {
@@ -45,19 +46,39 @@ var donate = (function () {
       });
 
       $(this).addClass('active');
+      amountToDonate = $(this).text().replace('$', '').trim();
     });
 
-    $('#donation-form #custom-amount').keypress(function () {
-      console.log('changed');
+    $('#donation-form #custom-amount').keydown(function () {
       $('#donation-form .error').css('visibility', 'hidden');
     });
 
+    $('#donation-form #custom-amount').keydown(function (e) {
+      // Allow: backspace, delete, tab, escape, enter and .
+      if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+        // Allow: Ctrl/cmd+A
+        (e.keyCode == 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: Ctrl/cmd+C
+        (e.keyCode == 67 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: Ctrl/cmd+X
+        (e.keyCode == 88 && (e.ctrlKey === true || e.metaKey === true)) ||
+        // Allow: home, end, left, right
+        (e.keyCode >= 35 && e.keyCode <= 39)) {
+        // let it happen, don't do anything
+        return;
+      }
+      // Ensure that it is a number and stop the keypress
+      if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+      }
+    });
+
     $('#donation-form #continue').click(function () {
-      console.log($('#custom-amount').val());
-      if ($('.donation-level.active li.active').length <= 0 && $('#custom-amount').val().length <= 0) {
+      if ($('.donation-level.active li.active').length <= 0 && $('#custom-amount').val().trim().length <= 0) {
         $('#donation-form .error').css('visibility', 'visible');
       } else {
-        // send donate value over
+        var amountToChargeCard = $('#custom-amount').val().trim().length > 0 ? $('#custom-amount').val().trim() : amountToDonate;
+        console.log (amountToChargeCard);
       }
     });
   }
