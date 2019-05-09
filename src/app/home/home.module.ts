c1502ec692c7d1daa39fbb3ff5d3a9d144c8e6ba
Alex Kopen <alexkopen@gmail.com>
2019-05-09 16:56:59 -0600

    fully functional slideshow

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CarouselComponent } from './carousel/carousel.component';
-import {SWIPER_CONFIG, SwiperConfigInterface, SwiperModule} from 'ngx-swiper-wrapper';
+import {
+  SWIPER_CONFIG,
+  SwiperConfigInterface,
+  SwiperModule
+} from 'ngx-swiper-wrapper';
 
 const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
   direction: 'horizontal',
   slidesPerView: 1,
-  autoplay: {delay: 4500},
+  autoplay: { delay: 5000 },
   allowTouchMove: false,
   speed: 1000
 };
@@ -20,7 +24,7 @@ const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
     RouterModule.forChild([
       { path: '', component: HomeComponent, pathMatch: 'full' }
     ]),
-      SwiperModule
+    SwiperModule
   ],
   providers: [
     {
