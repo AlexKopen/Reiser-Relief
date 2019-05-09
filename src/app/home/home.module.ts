3fc7bc25a3e2b0cb5086db11b4921d7ddf6a7cc0
Alex Kopen <alexkopen@gmail.com>
2019-05-09 15:45:38 -0600

    image sliding

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,14 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import {SWIPER_CONFIG, SwiperConfigInterface, SwiperModule} from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  autoplay: {delay: 4500},
+  allowTouchMove: false,
+  speed: 1000
+};
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, CarouselComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: '', component: HomeComponent, pathMatch: 'full' }
-    ])
+    ]),
+      SwiperModule
+  ],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
   ]
 })
 export class HomeModule {}
