37640462a543d42ed2832373b43ff43c5c838c7e
Alex Kopen <alexkopen@gmail.com>
2019-06-19 15:57:40 -0600

    some misc tweaks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(value => {
+      if (value instanceof NavigationStart) {
+        if (isPlatformBrowser(this.platformId)) {
+          window.scrollTo(0, 0);
+        }
+      }
+    });
+  }
+}
