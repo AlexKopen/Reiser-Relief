604a1b99c21d6573b870db7b90740e7e34f90537
Alex Kopen <alexkopen@gmail.com>
2019-08-02 13:15:02 -0600

    prettier

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyB4CEiPDHYwLdP7SJdaBRS93NKg0s5yr4Q",
-    authDomain: "reiser-relief-test.firebaseapp.com",
-    databaseURL: "https://reiser-relief-test.firebaseio.com",
-    projectId: "reiser-relief-test",
+    apiKey: 'AIzaSyB4CEiPDHYwLdP7SJdaBRS93NKg0s5yr4Q',
+    authDomain: 'reiser-relief-test.firebaseapp.com',
+    databaseURL: 'https://reiser-relief-test.firebaseio.com',
+    projectId: 'reiser-relief-test',
     storageBucket: "reiser-relief-test.appspot.com'",
-    messagingSenderId: "443652747248"
+    messagingSenderId: '443652747248'
   }
 };
