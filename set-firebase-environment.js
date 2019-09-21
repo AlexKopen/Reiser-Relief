3ae568e8936d31057b46685e04f01681c9758bb1
Alex Kopen <alexkopen@gmail.com>
2019-09-21 12:15:16 -0600

    environment replace test

---
+++ set-firebase-environment.js
@@ -0,0 +1,9 @@
+const writeJsonFile = require('write-json-file');
+
+(async () => {
+    await writeJsonFile('.firebaserc', {
+        "projects": {
+            "default": "reiser-relief-test"
+        }
+    });
+})();
