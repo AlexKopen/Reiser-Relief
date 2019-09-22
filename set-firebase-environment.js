7bb70cff378ba67fb5d326399dd42c0a62d17dfc
Alex Kopen <alexkopen@gmail.com>
2019-09-22 16:25:03 -0600

    header updates

--- set-firebase-environment.js
+++ set-firebase-environment.js
@@ -1,9 +1,9 @@
 const writeJsonFile = require('write-json-file');
 
 (async () => {
-    await writeJsonFile('.firebaserc', {
-        "projects": {
-            "default": "reiser-relief-test"
-        }
-    });
+  await writeJsonFile('.firebaserc', {
+    projects: {
+      default: 'reiser-relief-test'
+    }
+  });
 })();
