929ec78348a7891fb14a348f43ae9dcaf197f7b8
Alex Kopen <alexkopen@gmail.com>
2019-10-04 15:33:55 -0600

    Combined news and events

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { BannerComponent } from './banner/banner.component';
-import { HomeCardsComponent } from './home-cards/home-cards.component';
-import { NewsletterSubscribeComponent } from './newsletter-subscribe/newsletter-subscribe.component';
-import { NewsPostsComponent } from './news-posts/news-posts.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { EventListingsComponent } from './event-listings/event-listings.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {BannerComponent} from './banner/banner.component';
+import {HomeCardsComponent} from './home-cards/home-cards.component';
+import {NewsletterSubscribeComponent} from './newsletter-subscribe/newsletter-subscribe.component';
+import {NewsPostsComponent} from './news-posts/news-posts.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,8 +14,7 @@ import { EventListingsComponent } from './event-listings/event-listings.componen
     BannerComponent,
     HomeCardsComponent,
     NewsletterSubscribeComponent,
-    NewsPostsComponent,
-    EventListingsComponent
+    NewsPostsComponent
   ],
   imports: [
     CommonModule,
