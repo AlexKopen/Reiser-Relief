03a9ab7bcf7d67cb16d483804f8c97dd81e8d677
Alex Kopen <alexkopen@gmail.com>
2019-10-19 15:53:25 -0600

    some more circleci environment touchups

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment = {
     authDomain: 'reiser-relief-test.firebaseapp.com',
     databaseURL: 'https://reiser-relief-test.firebaseio.com',
     projectId: 'reiser-relief-test',
-    storageBucket: "reiser-relief-test.appspot.com'",
+    storageBucket: 'reiser-relief-test.appspot.com',
     messagingSenderId: '443652747248'
   }
 };
